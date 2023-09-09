4d2821eebb221cfd51f68f1663c4b98f36b744d1
fkf <2338699518@qq.com>
2023-09-09 10:12:03 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-  return 'Hello World!'
+# @app.route('/')
+# def hello():
+#   return 'Hello World!'
+from markupsafe import escape
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
